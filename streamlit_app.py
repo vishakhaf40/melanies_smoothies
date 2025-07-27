d7be8a1475cd764951165e1ed60eb3cc9d22f721
vishakhaf40 <vishakhaf40@gmail.com>
2025-07-27 13:44:31 +0530

    Update streamlit_app.py
    FRUITYVICE.com

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://FRUITYVICE.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
 
 # Write directly to the app
